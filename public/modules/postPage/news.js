85a252be87882a43642cba1bd9c253a9f700811d
salome <salomemumladze775@gmail.com>
2021-07-02 20:27:27 +0400

    add olympic page

--- public/modules/postPage/news.js
+++ public/modules/postPage/news.js
@@ -1,55 +1,55 @@
 let newthings = [
   {
     id: 1,
-    img: "../img/1.png",
+    img: "../../img/1.png",
     title: "სკანდალი ჩოგბურთში - რუსი იანა სიზიკოვა საფრანგეთში დააკავეს",
     caption: "3 საათის წინ",
   },
   {
     id: 2,
-    img: "../img/Rectangle28.png",
+    img: "../../img/Rectangle28.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 3,
-    img: "../img/Rectangle29.png",
+    img: "../../img/Rectangle29.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 4,
-    img: "../img/Rectangle30.png",
+    img: "../../img/Rectangle30.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 5,
-    img: "../img/Rectangle31.png",
+    img: "../../img/Rectangle31.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 6,
-    img: "../img/Rectangle32.png",
+    img: "../../img/Rectangle32.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 7,
-    img: "../img/Rectangle33.png",
+    img: "../../img/Rectangle33.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 5,
-    img: "../img/Rectangle34.png",
+    img: "../../img/Rectangle34.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
   {
     id: 6,
-    img: "../img/Rectangle35.png",
+    img: "../../img/Rectangle35.png",
     title: "კოუტინიო ვალის სანაცვლოდ - ბარსელონას უცნაური სქემა",
     caption: "3 საათის წინ",
   },
@@ -57,12 +57,12 @@ let newthings = [
 
 document.getElementById("all").innerHTML = newthings
   .map(
-    (newthing) => ` <a href="#" class="xxs:flex items-center xxs:my-5">
-    <img class="w-full rounded xxs:w-52 mr-5" src=${newthing.img}>
-    <div>
+    (newthing) => ` <a href="#" class=" h-80 xxs:flex items-center xxs:my-5">
+    <img class="w-full h-auto rounded xxs:w-52 mr-5" src=${newthing.img}>
+    <div class="grid h-24">
         <h2 class="text-black my-2.5 font-semibold leading-5 text-13 m-auto flex items-center">${newthing.title}</h2>
         <div class="flex items-center">
-            <img class="mr-1.5" src="../img/Clock.png"/>
+            <img class="mr-1.5" src="../../img/Clock.png"/>
             <h2 class="caption" style="color: #B2B2B2">3 საათის წინ</h2>
         </div>
     </div>
--- public/modules/postPage/post.js
+++ public/modules/postPage/post.js
@@ -1,8 +1,8 @@
 function showWishlist() {
   var x = document.getElementById("list_box");
   var title = document.getElementById("nav_title");
-  var img1 = "../img/arrow.png";
-  var img2 = "../img/orangeArrow.png";
+  var img1 = "../../img/arrow.png";
+  var img2 = "../../img/orangeArrow.png";
   var imgElement = document.getElementById("arrow");
 
   if (x.style.display === "block" && title.style.color === "red") {
